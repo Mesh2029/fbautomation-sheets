64fc965cacc94a179d69e355ef53d910a9d45101
Yegon <meshcode75@gmail.com>
2025-08-29 20:36:13 +0300

    google apis calls

--- listonmarketplace.js
+++ listonmarketplace.js
@@ -18,7 +18,7 @@ const axios = require('axios');
 const APPSHEET_APP_ID = '41aa9d8f-9048-4ab7-ad08-60bac0a43488';
 // const APPSHEET_TABLE_ID = 'wafbproducts';
 const APPSHEET_TABLE_ID = 'Sheet1';
-const APPSHEET_API_KEY = 'q7IoB-iZwUV-mxZYq-SHhHA-2youF-oTAXC-ZmeWd-ZzYsp';
+const APPSHEET_API_KEY = 'V2-zcaLi-OIn17-hF5Dx-jJ3g8-SBkx2-MuHXG-pnljU-AF0rS';
 
 
 
